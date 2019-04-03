f4710e142dab7f9c8d25770ebf42ea65fb5f43cb
Alan Accurso <accurso.alan@gmail.com>
2019-04-03 17:46:38 -0300

    Initial commit

---
+++ src/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+// Create a <Wrapper> react component that renders a <section>
+// with some padding and a papayawhip background
+export default styled.section`
+  padding: 4em;
+  background: papayawhip;
+  display: flex;
+  height: calc(100% - 8em);
+  flex-direction: column;
+`;
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { render } from "react-dom";
+import styled, { createGlobalStyle } from "styled-components";
+
+import Wrapper from "./Wrapper";
+import Title from "./Title";
+
+const GlobalStyle = createGlobalStyle`
+body, html, #root {
+  margin: 0;
+  height: 100%;
+}
+`;
+
+const Body = styled.div`
+  background: black;
+  flex-grow: 1;
+  overflow-y: scroll;
+`;
+
+// Render these styled components like normal react components.
+// They will pass on all props and work
+// like normal react components – except they're styled!
+const App = () => (
+  <Wrapper>
+    <GlobalStyle />
+    <Title primary>Hello World, this is my first styled component!</Title>
+    <Body>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+      <Title>Hello World, this is my first styled component!</Title>
+    </Body>
+    <Title primary>Hello World, this is my first styled component!</Title>
+  </Wrapper>
+);
+
+render(<App />, document.getElementById("root"));
